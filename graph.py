5fdb2fb50dbebe025d301d60b0531048bed1dcb4
nilayjain <nilayjain13@gmail.com>
2016-11-28 13:37:37 +0000

    graph script added

--- graph.py
+++ graph.py
@@ -1,15 +1,17 @@
 import time
 import libvirt
-f= open("guru99.txt","w+")
+f= open("data.txt","w+")
 conn = libvirt.open('qemu:///system')
 if conn == None:
 		print('Failed to open connection to qemu:///system')
 while True:
+	sec = int(time.time())
 	mem= conn.getFreeMemory()
 	domainIDs = conn.listDomainsID()
 	no_of_VM = len(domainIDs)
 	stats = conn.getCPUStats(0)
-	str1 = str(mem)+' '+str(no_of_VM)+' '+str(stats)
+	str1 = str(sec) + ' '+str(mem)+' '+str(no_of_VM)+' '+str(stats)+'\n'
 	f.write(str1)
+	print '.'
 	time.sleep(2)
 f.close
